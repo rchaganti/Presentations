41819c574053638b66548a80bdda4ce45d898dc2
Ravikanth C <ravikanth@ravichaganti.com>
2018-04-11 21:37:20 +0530

    Update 411

---
+++ psconfeu2018/SHiPS/DemoScripts/02-Demo_DellPESCPDrive.ps1
@@ -0,0 +1,12 @@
+﻿$credential = Get-Credential
+
+$testHash = @{
+    Path =  'D:\Github\DellPESCPDrive\Tests\DellPESCPDrive.Sample2.Tests.ps1'
+    Parameters = @{
+        referenceSystemDRAC  = '172.16.100.23'
+        DRACCredential       = $credential
+        differenceSystemDRAC = @('172.16.100.24')
+    }
+}
+
+Invoke-Pester -Script $testHash -OutputFormat NUnitXml -OutputFile D:\Tools\TestResult-CompareSystem.xml
