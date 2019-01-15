0147c801237797620945397e4b8ec8c3960bd9c2
rchaganti <Ravikanth@Ravichagant.com>
2019-01-15 13:34:44 +0530

    updating ELIND slides

---
+++ ExpertsLiveIN2019/Demo/AzModule/01-Demo.ps1
@@ -0,0 +1,9 @@
+﻿# Install the Az Module
+# This is a rollup module which downloads and installs other PowerShell modules for Azure services
+Install-Module -Name Az -Force
+
+# Get all Azure modules
+Get-Module -ListAvailable -Name Az*
+
+# Connect Azure Account
+Get-Help Connect-AzAccount -Online
