3a34c168927c42c28a55ec6bde3a2449e81c5923
rchaganti <Ravikanth@Ravichagant.com>
2019-01-18 11:56:51 +0530

    updates to shell script

--- ExpertsLiveIN2019/Demo/AzCli/01-Demo.sh
+++ ExpertsLiveIN2019/Demo/AzCli/01-Demo.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
 
 ## Login
-az Login
+### Interactive
+az login
+
+### Service Principal
+az login --service-principal -u "91a8ad81-2f20-4800-8b1d-e6299f20b533" --tenant "7eb05ed1-e512-43a8-b91e-bcf3f53904f2"
 
 ## Finding az commands
 az find -q vm
 az -help
+
+## Get vm image list
+az vm image list
+
+## Get VM image skus
+az vm image list-skus --help
+az vm image list-skus -p MicrosoftWindowsServer -f windowsserver
+
+## Create a vm
+az vm create -n w2016vm01 -g s2d --image Win2016Datacenter
 
 ## az output formats
 az account list-locations -o json
