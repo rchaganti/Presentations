350a8a00261824d99433f97b83697b683b81a80a
rchaganti <ravikanth@ravichaganti.com>
2019-06-05 16:01:49 +0530

    Create 01-PublishModule.ps1

---
+++ psconfeu2019/Community/Demo/06-PublishModule/01-PublishModule.ps1
@@ -0,0 +1,6 @@
+$apiKey = 'oy2emcbbfcqqpetwhwjavsin666if4wuzl5adiuhimugee'
+
+Get-Module -ListAvailable -Name PSWindowsAdminCenter
+Get-Command -Module PSWindowsAdminCenter
+
+Publish-Module -Name PSWindowsAdminCenter -NuGetApiKey $apiKey
